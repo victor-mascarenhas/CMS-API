4749417caf9c5e96c1f75a6db7c7028ac21900d0
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2020-12-03 16:29:53 -0300

    finalizando

---
+++ routes/api/email.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer')
+
+
+router.post('/', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "a0e619621fe632",
+            pass: "86c2ca55b8b003"
+        }
+    });
+
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'destinatario@teste.com',
+        subject: `Esse é um email teste ${req.body.name} `,
+        text: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('#### ERROR =>', error)
+            res.json({ type: 'danger', message: 'Erro eo enviar o e-mail', data: error })
+        } else {
+            res.json({
+                type: 'success',
+                message: 'E-mail enviado com sucesso',
+                data: info.respo
+            })
+        }
+    });
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use('/services', require('./routes/api/services'))
 app.use('/info', require('./routes/api/info'))
 app.use('/user', require('./routes/api/user'))
 app.use('/auth', require('./routes/api/auth'))
+app.use('/email', require('./routes/api/email'))
 
 
 app.listen(PORT, () => {console.log(`Listening on: ${PORT}`)})
